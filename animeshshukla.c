51302253641a7169ef59ed07da902c56994d1f26
animeshshukla2005-alt <animeshshukla2005@gmail.com>
2025-08-28 12:03:37 +0530

    perimeter and area of rectangle

---
+++ animeshshukla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int length, width;
+    int area, perimeter;
+    printf("Enter the length of the rectangle: \n");
+    scanf("%d", &length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+    area = length * width;
+    perimeter = 2 * (length + width);
+    printf("The area of the rectangle is: %d\n", area);
+    printf("The perimeter of the rectangle is: %d\n", perimeter);
+
+}
