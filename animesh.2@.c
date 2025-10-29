427ad6eb2f1ee263e0bd160119b0b00034d4c80e
animeshshukla2005-alt <animeshshukla2005@gmail.com>
2025-10-29 16:19:40 +0530

    program to take 10 integers from user and print even value numbers

---
+++ animesh.2@.c
@@ -0,0 +1,24 @@
+//program to take 10 integers from user and print even value number
+//Developed by Animesh Date:29/10/2025
+#include <stdio.h>
+int main()
+{
+    int numbers[10];
+    int i;
+    printf("Enter 10 integer values:\n");
+    for (i = 0; i < 10; i++)
+    {
+    printf(Enter number %d: ",i + 1);
+    scanf("%d", &numbers[i]);
+    }
+    printf("n\Even numbers entered:\n")
+    for (i = 0; i < 10; i++)
+    {
+     if (numbers[i] % 2 == 0)
+     {
+       printf("%d\n", numbers [1]);
+
+       }
+          }
+             }
+
